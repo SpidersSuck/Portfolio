f313f0813219ea10aa71fab9d88e8034d229860b
julian hutchins <93027694+SpidersSuck@users.noreply.github.com>
2025-12-08 02:22:02 -0800

    Configure GitHub Pages deployment for sprint-6

---
+++ project/build/pacman-game/src/sound.js
@@ -0,0 +1,112 @@
+/* Sound handlers added by Dr James Freeman who was sad such a great reverse was a silent movie  */
+
+var audio = new preloadAudio();
+
+// Expose audio globally for volume control
+window.gameAudio = audio;
+
+function audioTrack(url, volume) {
+    var audio = new Audio(url);
+    if (volume) audio.volume = volume;
+    audio.load();
+    var looping = false;
+    
+    // Expose the audio element for external volume control
+    this.audioElement = audio;
+    
+    this.setVolume = function(vol) {
+        try {
+            if (audio && typeof vol === 'number' && !isNaN(vol)) {
+                audio.volume = Math.max(0, Math.min(1, vol));
+            }
+        } catch (e) {
+            console.error('Error setting volume:', e);
+        }
+    };
+    
+    this.play = function(noResetTime) {
+        playSound(noResetTime);
+    };
+    this.startLoop = function(noResetTime) {
+        if (looping) return;
+        audio.addEventListener('ended', audioLoop);
+        audioLoop(noResetTime);
+        looping = true;
+    };
+    this.stopLoop = function(noResetTime) {
+        try{ audio.removeEventListener('ended', audioLoop) } catch (e) {};
+        audio.pause();
+        if (!noResetTime) audio.currentTime = 0;
+        looping = false;
+    };
+    this.isPlaying = function() {
+        return !audio.paused;
+    };
+    this.isPaused = function() {
+        return audio.paused;
+    }; 
+    this.stop = this.stopLoop;
+
+    function audioLoop(noResetTime) {
+        playSound(noResetTime);
+    }
+    function playSound(noResetTime) {
+        // for really rapid sound repeat set noResetTime
+        if(!audio.paused) {
+            audio.pause();
+            if (!noResetTime ) audio.currentTime = 0;
+        }
+        try{
+            var playPromise = audio.play();
+            if(playPromise) {
+                playPromise.then(function(){}).catch(function(err){});
+            }
+        } 
+        catch(err){ console.error(err) }
+    }
+}
+
+
+function preloadAudio() {
+
+    this.credit            = new audioTrack('sounds/credit.mp3');
+    this.coffeeBreakMusic  = new audioTrack('sounds/coffee-break-music.mp3');
+    this.die               = new audioTrack('sounds/miss.mp3');
+    this.ghostReturnToHome = new audioTrack('sounds/ghost-return-to-home.mp3');
+    this.eatingGhost       = new audioTrack('sounds/eating-ghost.mp3');
+    this.ghostTurnToBlue   = new audioTrack('sounds/ghost-turn-to-blue.mp3', 0.5);
+    this.eatingFruit       = new audioTrack('sounds/eating-fruit.mp3');
+    this.ghostSpurtMove1   = new audioTrack('sounds/ghost-spurt-move-1.mp3');
+    this.ghostSpurtMove2   = new audioTrack('sounds/ghost-spurt-move-2.mp3');
+    this.ghostSpurtMove3   = new audioTrack('sounds/ghost-spurt-move-3.mp3');
+    this.ghostSpurtMove4   = new audioTrack('sounds/ghost-spurt-move-4.mp3');
+    this.ghostNormalMove   = new audioTrack('sounds/ghost-normal-move.mp3');
+    this.extend            = new audioTrack('sounds/extend.mp3');
+    this.eating            = new audioTrack('sounds/eating.mp3', 0.5);
+    this.startMusic        = new audioTrack('sounds/start-music.mp3');
+
+    // Add global setVolume method for all tracks
+    this.setGlobalVolume = function(vol) {
+        for (var key in this) {
+            if (this.hasOwnProperty(key) && 
+                typeof this[key] === 'object' && 
+                typeof this[key].setVolume === 'function') {
+                this[key].setVolume(vol);
+            }
+        }
+    };
+
+    this.ghostReset = function(noResetTime) {
+        for (var s in this) {
+            if (s == 'silence' || s == 'ghostReset' || s == 'setGlobalVolume') return;
+            if (s.match(/^ghost/)) this[s].stopLoop(noResetTime);
+        }
+    };
+
+    this.silence = function(noResetTime) {
+        for (var s in this) {
+            if (s == 'silence' || s == 'ghostReset' || s == 'setGlobalVolume') return;
+            this[s].stopLoop(noResetTime);
+        }
+    }
+}
